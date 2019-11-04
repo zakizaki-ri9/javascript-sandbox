729a423f5a90c6a0543911910953912c24a70b64
lanches-yazaki-yuki <yazaki.yuuki@lanches.co.jp>
2019-11-05 08:35:34 +0900

    fix: eslint

--- react-sample/.eslintrc.js
+++ react-sample/.eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:prettier/recommended',
     'plugin:react/recommended',
-    'plugin:jest/recommended'
+    'plugin:jest/recommended',
+    'react-app'
   ],
   globals: {
     Atomics: 'readonly',
