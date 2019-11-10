f542983f2ea71cd0f1f6c7194b8e669d0180c98c
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-11-10 22:15:17 +0900

    refactor: emotion style

--- react-webpack/src/animation/hoverBox.jsx
+++ react-webpack/src/animation/hoverBox.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import { css } from '@emotion/core'
 
-const HoverBox = () => {
-  return (
-    <div
-      css={css({
-        width: '2%',
-        height: '100vh',
-        background: '#000',
-        opacity: 0.1,
-        transition: 'all 300ms 0s ease',
+const style = css`
+  width: 2%;
+  height: 100vh;
+  background: #000;
+  opacity: 0.1;
+  transition: all 300ms 0s ease;
 
-        ':hover': {
-          width: '15%',
-          opacity: 1
-        }
-      })}
-    ></div>
-  )
+  &:hover {
+    width: 15%;
+    opacity: 1;
+  }
+`
+
+const HoverBox = () => {
+  return <div css={style}></div>
 }
 
 export default HoverBox
