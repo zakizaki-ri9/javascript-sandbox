d86b8bbcbebac01b8c6b0672db5b4b30133e7a1a
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-11-27 16:03:59 +0900

    feat: graphql コメント追記

--- graphql-nodejs/src/hello-world.js
+++ graphql-nodejs/src/hello-world.js
@@ -1,25 +1,34 @@
 let { graphql, buildSchema } = require("graphql");
 
+// スキーマを定義
 let schema = buildSchema(`
   type Query {
     hello: String
+    test: String
   }
 `);
 
+// 取得するクエリを定義
 let root = {
   hello: () => {
     return "Hello world!";
+  },
+  test: () => {
+    return "Test";
   }
 };
 
+// スキーマに対しての問い合わせ
 graphql(
   schema,
   `
     {
       hello
+      test
     }
   `,
   root
 ).then(response => {
+  // 結果取得
   console.log(response);
 });
