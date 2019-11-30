7004fba9a182f92b1f0d2b2191b0cd1ebedb418c
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-11-30 16:03:29 +0900

    feat: toolbarに合わせてheightを自動調整するロジックを実装

--- vue-material-v1-sandbox/src/App.vue
+++ vue-material-v1-sandbox/src/App.vue
@@ -1,16 +1,14 @@
 <template>
-  <md-app md-mode="reveal">
-    <md-app-toolbar class="md-primary">
+  <!-- <main>
+    <md-toolbar class="md-primary">
       <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
         <md-icon>menu</md-icon>
       </md-button>
       <span class="md-title">Vue Material v1 Practice</span>
-    </md-app-toolbar>
+    </md-toolbar>
 
-    <md-app-drawer :md-active.sync="menuVisible" style="width: 300px;">
-      <md-toolbar class="md-transparent" md-elevation="0">
-        SideNav
-      </md-toolbar>
+    <md-drawer :md-active.sync="menuVisible" style="width: 300px;">
+      <md-toolbar class="md-transparent" md-elevation="0">SideNav</md-toolbar>
 
       <md-list>
         <template v-for="(menu, i) in menus">
@@ -22,12 +20,37 @@
           </md-list-item>
         </template>
       </md-list>
-    </md-app-drawer>
+    </md-drawer>
 
-    <md-app-content>
+    <md-content class="container">
       <router-view />
-    </md-app-content>
-  </md-app>
+    </md-content>
+  </main>-->
+  <main style="height: 100vh; width: 100%;">
+    <md-toolbar class="md-primary" ref="toolbar">
+      <span class="md-title">Vue Material v1 Practice</span>
+    </md-toolbar>
+    <div class="base flex" :style="baseStyles">
+      <md-drawer md-permanent="clipped">
+        <md-toolbar class="md-transparent" md-elevation="0">SideNav</md-toolbar>
+
+        <md-list>
+          <template v-for="(menu, i) in menus">
+            <md-list-item v-if="!!menu.label" :key="i">
+              <md-icon>{{ menu.mdIcon }}</md-icon>
+              <router-link :to="menu.path" @click.native="menuVisible = false">
+                <span class="md-list-item-text">{{ menu.label }}</span>
+              </router-link>
+            </md-list-item>
+          </template>
+        </md-list>
+      </md-drawer>
+
+      <div class="container">
+        <router-view />
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -35,13 +58,57 @@ import { routes } from './router'
 export default {
   data() {
     return {
-      menuVisible: false
+      menuVisible: false,
+      toolbarHeight: 0
     }
   },
   computed: {
     menus() {
       return routes
+    },
+    /**
+     * div.base.flex のスタイル
+     * @returns {Object}
+     */
+    baseStyles() {
+      // vue-materialのmd-toolbarの高さ変動に合わせて調整
+      let height = this.toolbarHeight
+        ? `calc(100% - ${this.toolbarHeight}px)`
+        : '100%'
+      return {
+        height: height
+      }
     }
+  },
+  methods: {
+    onWindowResize() {
+      this.toolbarHeight = this.$refs.toolbar.$el.clientHeight
+    }
+  },
+  mounted() {
+    window.addEventListener('resize', this.onWindowResize)
+    this.onWindowResize()
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onWindowResize)
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.base {
+  margin: 0px;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+
+  .container {
+    @extend .base;
+    padding: 5px;
+  }
+}
+.flex {
+  display: flex;
+}
+</style>
