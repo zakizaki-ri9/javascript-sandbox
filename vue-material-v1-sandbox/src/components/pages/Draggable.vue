6a9721515b02e432e6b404d49373f17506885859
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-12-24 14:35:57 +0900

    feat: draggable md-table

--- vue-material-v1-sandbox/src/components/pages/Draggable.vue
+++ vue-material-v1-sandbox/src/components/pages/Draggable.vue
@@ -5,6 +5,36 @@
         {{ str }}
       </md-chip>
     </draggable>
+    <draggable tag="md-table" :list="objectList">
+      <!-- <md-table> -->
+      <md-table-row v-for="(obj, index) in objectList" :key="index">
+        <md-table-cell>{{ obj.first }}</md-table-cell>
+        <md-table-cell>{{ obj.last }}</md-table-cell>
+      </md-table-row>
+      <!-- </md-table> -->
+    </draggable>
+    <div class="md-content md-table md-theme-default">
+      <div class="md-content md-table-content md-scrollbar md-theme-default">
+        <draggable tag="table" v-model="objectList">
+          <tr
+            v-for="(obj, index) in objectList"
+            :key="index"
+            class="md-table-row"
+          >
+            <td class="md-table-cell">
+              <div class="md-table-cell-container">
+                {{ obj.first }}
+              </div>
+            </td>
+            <td class="md-table-cell">
+              <div class="md-table-cell-container">
+                {{ obj.last }}
+              </div>
+            </td>
+          </tr>
+        </draggable>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,11 +51,34 @@ export default {
   },
   data() {
     return {
-      strList: ['aaa', 'bbb', 'ccc']
+      strList: ['aaa', 'bbb', 'ccc'],
+      objectList: [
+        { first: 'aaa', last: 'bbb' },
+        { first: 'ccc', last: 'ddd' },
+        { first: 'eee', last: 'fff' }
+      ]
     }
   },
   methods: {}
 }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.md-table-row {
+  /* transition: 0.3s $md-transition-default-timing;
+  transition-property: background-color, font-weight;
+  will-change: background-color, font-weight; */
+
+  &.md-has-selection {
+    cursor: pointer;
+  }
+
+  &.md-selected-single {
+    font-weight: 500;
+  }
+
+  tbody & td {
+    border-top: 1px solid;
+  }
+}
+</style>
