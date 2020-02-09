8c5c4a5e2d8150c3226c1182a56bd97a399f931b
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-02-10 08:49:49 +0900

    fetch connpass api

--- leaflet-typescript-practice/src/views/ClickMap.vue
+++ leaflet-typescript-practice/src/views/ClickMap.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
-    <l-map
-      ref="map"
-      style="height: 600px;"
-      :zoom="zoom"
-      :center="center"
-      @click="onClickMap"
-    >
+    <v-btn :loading="loading" @click="search">検索</v-btn>
+    <l-map ref="map" style="height: 600px;" :zoom="zoom" :center="center">
       <l-tile-layer :url="url"></l-tile-layer>
       <l-marker
         v-for="(marker, index) in markers"
@@ -22,6 +17,8 @@
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
 import { Icon, LatLng } from 'leaflet'
+import axios from 'axios'
+import dayjs from 'dayjs'
 
 @Component({
   components: { LMap, LTileLayer, LMarker }
@@ -34,10 +31,40 @@ export default class LeafletMap extends Vue {
   icon: Icon = new Icon({
     iconUrl: require('leaflet/dist/images/marker-icon.png')
   })
+  loading = false
+
+  get connpassUrl(): string {
+    return 'https://connpass.com/api/v1/event'
+  }
+
+  // クリックしたらマーカーセットするサンプルメソッド
   onClickMap(e: { latlng: LatLng }) {
     if (e && e.latlng) {
       this.markers.push(e.latlng)
     }
+  }
+
+  search() {
+    this.loading = true
+    axios
+      .get(this.connpassUrl, {
+        params: {
+          ymd: dayjs().format('YYYYMMDD')
+        },
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+      .then(res => {
+        // eslint-disable-next-line no-console
+        console.log({
+          res: res
+        })
+      })
+      .finally(() => {
+        this.loading = false
+      })
   }
 }
 </script>
