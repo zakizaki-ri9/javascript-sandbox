c7cb0f0ee958e7d859597557174ea8ae8d402e95
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-04-12 14:39:53 +0900

    feat: padding

---
+++ gridsome-trattoria-new-site-demo/src/components/atmos/GTxt.vue
@@ -0,0 +1,12 @@
+<template>
+  <p class="el_txt">
+    <slot />
+  </p>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="scss"></style>
--- gridsome-trattoria-new-site-demo/src/pages/Index.vue
+++ gridsome-trattoria-new-site-demo/src/pages/Index.vue
@@ -1,17 +1,19 @@
 <template>
   <Layout>
-    <g-h1>Hello, world!</g-h1>
+    <g-h1>Level1 Heading</g-h1>
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <p>
+    <g-txt>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
       excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
       Dolores, aperiam non officia eos quod asperiores
-    </p>
+    </g-txt>
 
-    <p class="home-links">
+    <g-h2>Level2 Heading</g-h2>
+
+    <!-- <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
         >Gridsome Docs</a
       >
@@ -21,16 +23,20 @@
         rel="noopener"
         >GitHub</a
       >
-    </p>
+    </p> -->
   </Layout>
 </template>
 
 <script>
 import Vue from 'vue'
 import GH1 from '~/components/atmos/GH1.vue'
+import GH2 from '~/components/atmos/GH2.vue'
+import GTxt from '~/components/atmos/GTxt.vue'
 export default Vue.extend({
   components: {
-    GH1
+    GH1,
+    GH2,
+    GTxt
   },
   metaInfo: {
     title: 'Hello, world!'
