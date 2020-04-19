7521f0046832d578e926254554a97cd0d5250966
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-04-19 11:27:39 +0900

    fix: eslint

--- gridsome-micro-cms-sample/gridsome.server.js
+++ gridsome-micro-cms-sample/gridsome.server.js
@@ -1,4 +1,25 @@
+// microCmSから
+const microCmsFetch = async () => {
+  require('dotenv').config()
+
+  const axios = require('axios')
+  const headers = {
+    'X-API-KEY': process.env.MICROCMS_API_KEY
+  }
+  return await axios.get(process.env.MICROCMS_URL, {
+    headers
+  })
+}
+
 module.exports = function (api) {
-  api.loadSource(() => {})
+  api.loadSource(async (actions) => {
+    const { data } = await microCmsFetch()
+
+    const collection = actions.addCollection('microCms')
+    collection.addNode({
+      context: data.context
+    })
+  })
+
   api.createPages(() => {})
 }
